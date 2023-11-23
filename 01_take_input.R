3bd00b651667da4bd09bb9f4cbfc142f2fcb3648
namburikrishnaganesh <123491310+namburikrishnaganesh@users.noreply.github.com>
2023-11-23 11:01:30 +0530

    Add files via upload

---
+++ 01_take_input.R
@@ -0,0 +1,4 @@
+name<-c("nani","chinnu")
+age<-c(20,42)
+df<-data.frame(name,age)
+print(df)
---
+++ 02_object_memory.R
@@ -0,0 +1,6 @@
+name<-c("nani","chinnu","minnu")
+age<-c(23,45,67)
+ls<-c(name)
+print(ls)
+ls<-c(age)
+print(ls)
